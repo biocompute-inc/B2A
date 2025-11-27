15aeb5448ed862256db0a7d0fb28878ab70c8868
ajaypavan1004 <ajaypavan1004@gmail.com>
2025-11-27 12:18:58 +0530

    Add files via upload

---
+++ get_stats.py
@@ -0,0 +1,26 @@
+import statistics
+
+def compute_stats(mfile="methylation_M.txt"):
+    meth_values = []
+
+    with open(mfile) as f:
+        for line in f:
+            pos, frac = line.split()
+            pos = int(pos)
+            frac = float(frac)
+
+            # filter for site specificity and  ONT baseline methylation
+            if pos >= 60 and pos % 12 == 0 and frac > 15.5:
+                meth_values.append(frac)
+
+    if not meth_values:
+        raise ValueError("No values passed the filters!")
+
+    mean_value = statistics.mean(meth_values)
+    median_value = statistics.median(meth_values)
+
+    # Print both so the shell script can capture them
+    print(mean_value, median_value)
+
+if __name__ == "__main__":
+    compute_stats()
